ae40fc370a953c93c82bf28f8cc3d458cbe6d440
yatsuhashi168 <kurimi777luk@gmail.com>
2021-12-03 17:27:19 +0900

    IDの昇順で並ぶようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.page(params[:page]).per(3)
+    @books = Book.order(id: 'ASC').page(params[:page]).per(3)
   end
 
   # GET /books/1
