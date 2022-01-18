0be2ba28e18b8133bee1ec2ce10da6e4f3abe348
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 14:01:46 +0900

    letter_opener_web用設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # ここから自分で書いた設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
