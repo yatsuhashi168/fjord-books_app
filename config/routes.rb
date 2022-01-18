d8792f5e075dc77f3c11eeedf46b3477a7053c6c
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 14:24:55 +0900

    routesを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   resources :books
+  resources :users, only: [:index, :show]
 end
