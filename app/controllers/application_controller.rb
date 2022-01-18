a6652b16f0920ee1708639402733b54e06bb15eb
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 14:12:46 +0900

    ログアウト後にサインインページへリダイレクトするよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
