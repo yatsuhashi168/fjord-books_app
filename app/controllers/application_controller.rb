85ba991a78528a5772bbc5b446385091e1a3ecab
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 17:02:06 +0900

    ストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :introduction])
+  end
+
   private
 
-  # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
