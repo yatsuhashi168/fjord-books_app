778c5d0fe0b5e55e807af8ca701ce8cef6323cb5
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 14:19:01 +0900

    rails g controller Users index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   resources :books
-
 end
