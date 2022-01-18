164f324ae330628d094d8425e20ad78a06051eda
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-18 15:33:20 +0900

    rootページ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root to: 'books#index'
   devise_for :users
   resources :books
   resources :users, only: [:index, :show]
