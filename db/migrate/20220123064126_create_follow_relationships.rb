95b1ac7ea96d59a51314db2469064ca4a77cdfde
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-23 15:46:16 +0900

    インデックスとユニーク制約を追加

--- db/migrate/20220123064126_create_follow_relationships.rb
+++ db/migrate/20220123064126_create_follow_relationships.rb
@@ -1,10 +1,13 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps
     end
+    add_index :follow_relationships, :follower_id
+    add_index :follow_relationships, :followed_id
+    add_index :follow_relationships, %i[follower_id followed_id], unique: true
   end
 end
