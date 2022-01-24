29057aaebb28d524b6e819987bd3de691c139d09
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-24 16:55:00 +0900

    FollowRelationshipにバリデーション追加

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,4 +1,6 @@
 class FollowRelationship < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+  validates :followed_id, presence: true, uniqueness: { scope: :follower_id }
+  validates :follower_id, presence: true
 end
