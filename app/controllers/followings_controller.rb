57f02fb98a92a1907592c0e9ad09f798600d44a3
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-24 16:30:30 +0900

    followingsのindexアクションを実装

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,5 +1,6 @@
 class FollowingsController < ApplicationController
   def index
-    @users = FollowRelationship.find_by(follower: params[:user_id])
+    @user = User.find(params[:user_id])
+    @users = @user.followings.order(:id).page(params[:page])
   end
 end
