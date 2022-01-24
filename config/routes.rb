37767bcf08da6eb9fdfa992135bc589b92f475e1
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-24 16:07:38 +0900

    followingsのルーティング作成、follow_relationshipsのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'followings/index'
-  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
-  scope module: :users do
+  resources :users, only: %i(index show) do
+    resources :followings, only: :index
+  end
+  namespace :users do
     resources :follow_relationships, only: %i(create destroy)
   end
 end
