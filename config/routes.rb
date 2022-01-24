ab9673dca3da925d914b3c2fcc18463061de7ee3
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-24 16:34:56 +0900

    followersのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'followers/index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show) do
     resources :followings, only: :index
+    resources :followers, only: :index
   end
   namespace :users do
     resources :follow_relationships, only: %i(create destroy)
