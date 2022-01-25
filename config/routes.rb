9d28c55100c82a4446a262272fb09cd964e752ae
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-25 16:43:04 +0900

    generated by rails g scaffold Comment body user:references commentable: references 不要なファイルを削除 ルーティング、マイグレーションファイルを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
       resources :followings, only: [:index]
       resources :followers, only: [:index]
     end
+  end
+  resources :books do
+    resources :comments
+  end
+  resources :reports do
+    resources :comments
   end
 end
