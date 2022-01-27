d4df22168c945fa4aebb76a3871b7749d634081f
yatsuhashi168 <kurimi777luk@gmail.com>
2022-01-27 14:54:24 +0900

    誤字修正

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -4,7 +4,7 @@ class Reports::CommentsController < CommentsController
   private
 
   def set_commentable
-    @commentable = Report.find(params[:book_id])
+    @commentable = Report.find(params[:report_id])
   end
 end
 
