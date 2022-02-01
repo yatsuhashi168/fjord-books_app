e4d66e3506a99c9bc5b4b0973a715d97b4ce1f81
yatsuhashi168 <kurimi777luk@gmail.com>
2022-02-01 23:56:46 +0900

    Userモデルのテスト作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#following?_and_#follow" do
+    alice = users(:Alice)
+    bob = users(:Bob)
+    assert_not alice.following?(bob)
+    alice.follow(bob)
+    assert alice.following?(bob)
+  end
+
+  test "#followed_by?" do
+    alice = users(:Alice)
+    bob = users(:Bob)
+    assert_not alice.followed_by?(bob)
+    bob.follow(alice)
+    assert alice.followed_by?(bob)
+  end
+
+  test "#unfollow" do
+    alice = users(:Alice)
+    bob = users(:Bob)
+    alice.follow(bob)
+    assert alice.following?(bob)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
+  end
+
+  test "#name_or_email" do
+    alice = users(:Alice)
+    assert_equal alice.email, alice.name_or_email
+    alice.name = alice
+    assert_equal alice.name, alice.name_or_email
+  end
 end
