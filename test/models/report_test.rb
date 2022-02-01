a70e2b7bb5616016d19a129c7d368cc3f6261585
yatsuhashi168 <kurimi777luk@gmail.com>
2022-02-01 23:56:39 +0900

    Reportモデルのテスト作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?" do
+    alice = users(:Alice)
+    bob = users(:Bob)
+    report_a = reports(:report_a)
+    assert report_a.editable?(alice)
+    assert_not report_a.editable?(bob)
+  end
+
+  test "#created_on" do
+    report_a = reports(:report_a)
+    assert_equal report_a.created_at.to_date, report_a.created_on
+    assert_not_equal report_a.created_at, report_a.created_on
+  end
 end
